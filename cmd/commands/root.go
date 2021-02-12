93f35f45728dcb5e309f49e5290aa112ad9a1526
lpmatos <luccapessoamatos@gmail.com>
2021-02-11 23:00:25 -0300

    feat: log definitions

--- cmd/commands/root.go
+++ cmd/commands/root.go
@@ -5,8 +5,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// Local flag logLevel - global log level.
-var logLevel string
+var (
+	logLevel      string
+	logFormat     string
+	disableColors bool
+)
 
 // RootCmd represents the base command when called without any subcommands.
 var RootCmd = &cobra.Command{
@@ -16,12 +19,13 @@ var RootCmd = &cobra.Command{
 
 👉😳👈 This is a pretty CLI that can find animes passing scene images
 `,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		logging.Setup(logFormat, logLevel, disableColors)
+	},
 }
 
 func init() {
-	logging.Setup()
-	RootCmd.PersistentFlags().StringVar(&logLevel, "log-level", "", "Set the logging level. One of: debug|info|warn|error")
-	RootCmd.PersistentFlags().StringVar(&logLevel, "log-format", "", "The formating of the logs. Available values: text, json, json-pretty")
-	RootCmd.PersistentFlags().StringVar(&logLevel, "log-file", "", `The file where all logs should be printed to. "-" means stdout`)
-	RootCmd.AddCommand(VersionCmd())
+	RootCmd.PersistentFlags().StringVar(&logLevel, "log-level", "debug", "Set the logging level. One of: debug|info|warn|error")
+	RootCmd.PersistentFlags().StringVar(&logFormat, "log-format", "json", "The formating of the logs. Available values: text|json|json-pretty|plain")
+	RootCmd.PersistentFlags().BoolVar(&disableColors, "disable-colors", false, `Set true if you want disable colors of log output`)
 }
--- internal/helpers/default.go
+++ internal/helpers/default.go
@@ -1,6 +1,9 @@
 package helpers
 
-import "os"
+import (
+	"os"
+	"strings"
+)
 
 // FileExists function - check with a file exist in system.
 func FileExists(file string) bool {
@@ -9,4 +12,15 @@ func FileExists(file string) bool {
 		return false
 	}
 	return !info.IsDir()
+}
+
+// IsEmpty function - check if a string is empty.
+func IsEmpty(value string) bool {
+	return len(strings.TrimSpace(value)) == 0
+}
+
+// FormatFilePath function - just trimmet everything before the file name and added a line number in the end.
+func FormatFilePath(path string) string {
+	arr := strings.Split(path, "/")
+	return arr[len(arr)-1]
 }
