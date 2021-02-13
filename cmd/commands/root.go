38eec9ed42dd30d992d4e880e6b7a00951156c97
lpmatos <luccapessoamatos@gmail.com>
2021-02-13 08:41:22 -0300

    fix: refact log setup

--- cmd/commands/root.go
+++ cmd/commands/root.go
@@ -2,15 +2,14 @@ package commands
 
 import (
 	"github.com/lpmatos/loli/internal/logging"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 var (
-	logLevel      string
-	logFormat     string
-	logTarget     string
-	logFile       string
-	disableColors bool
+	logLevel  string
+	logFormat string
+	logOutput string
 )
 
 // RootCmd represents the base command when called without any subcommands.
@@ -22,14 +21,16 @@ var RootCmd = &cobra.Command{
 👉😳👈 This is a pretty CLI that can find animes passing scene images
 `,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		logging.Setup(logTarget, logFile, logFormat, logLevel, disableColors)
+		// init log options from command line params
+		err := logging.InitLog(logLevel, logFormat, logOutput)
+		if err != nil {
+			logrus.Warn("Error setting log level, using debug as default")
+		}
 	},
 }
 
 func init() {
 	RootCmd.PersistentFlags().StringVar(&logLevel, "log-level", "debug", "Set the logging level. One of: debug|info|warn|error")
 	RootCmd.PersistentFlags().StringVar(&logFormat, "log-format", "text", "The formating of the logs. Available values: text|json|json-pretty|plain")
-	RootCmd.PersistentFlags().StringVar(&logTarget, "log-target", "stdout", "Defaulting to Stdout. Available values: stdout|stderr|file")
-	RootCmd.PersistentFlags().StringVar(&logFile, "log-file", "output.log", "Defaulting to log file")
-	RootCmd.PersistentFlags().BoolVar(&disableColors, "disable-colors", false, `Set true if you want disable colors of log output`)
+	RootCmd.PersistentFlags().StringVar(&logOutput, "log-output", "stdout", "Defaulting to Stdout. Available values: stdout|stderr|file")
 }
