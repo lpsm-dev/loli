eb0d3ecb1c1350f025599de2921c64c05d6d203c
lpmatos <luccapessoamatos@gmail.com>
2021-02-21 21:43:17 -0300

    fix: constant trace moe url comment

--- internal/constants/default.go
+++ internal/constants/default.go
@@ -3,5 +3,6 @@ package constants
 const (
 	// DefaultTimestampFormat default time format
 	DefaultTimestampFormat = "2006-01-02_15:04:05"
-	TraceMoeSearchURL      = "https://trace.moe/api/search"
+	// TraceMoeSearchURL default trace.moe URL
+	TraceMoeSearchURL = "https://trace.moe/api/search"
 )
