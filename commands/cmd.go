c3e654164a158cec362cf86ead69665050c87fe1
Lucca Pessoa <luccapsm@protonmail.com>
2021-10-06 15:46:50 -0300

    fix: command cmg check release

--- commands/cmd.go
+++ commands/cmd.go
@@ -5,11 +5,9 @@ import (
 	"io"
 
 	"github.com/common-nighthawk/go-figure"
-	"github.com/lpmatos/loli/internal/cli"
 	"github.com/lpmatos/loli/internal/constants"
 	log "github.com/lpmatos/loli/internal/log"
 	"github.com/lpmatos/loli/internal/utils"
-	"github.com/lpmatos/loli/internal/version"
 )
 
 var (
@@ -23,7 +21,6 @@ var (
 // This is called by loli main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	figure.NewColorFigure(constants.BinaryName, "smslant", "yellow", false).Print()
-	c := cli.New(version.GetVersionFormatted())
 
 	outputRender, err := utils.RenderMarkdown(constants.Welcome)
 	if err != nil {
@@ -33,19 +30,5 @@ func Execute() {
 
 	if err := RootCmd.Execute(); err != nil {
 		log.Fatal("Error while executing RootCmd")
-	}
-
-	checkUpdate(c)
-}
-
-// checkUpdate function.
-func checkUpdate(c cli.Updater) {
-	ok, err := c.IsUpToDate()
-	if err != nil {
-		log.Error(err)
-	}
-
-	if !ok {
-		fmt.Println("\nThere is a new version!")
 	}
 }
