f3d4f2dd35dccea332c76bad8827c4ac789e491e
lpmatos <lpsm-dev@protonmail.com>
2022-01-03 15:20:12 -0300

    fix: setup command get last tag

--- scripts/variables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CLIENT_VERSION="$(cat VERSION)"
-GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-GO_VERSION="$(go version)"
-export CLIENT_VERSION
-export GIT_BRANCH
-export GO_VERSION
