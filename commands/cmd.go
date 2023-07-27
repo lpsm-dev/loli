3fd336481f5de28d3dcd043c52b63e449573d25e
lpsm-dev-github <lpsm-dev@protonmail.com>
2023-07-27 16:49:26 -0300

    fix: code with new comments and changes

--- commands/cmd.go
+++ commands/cmd.go
@@ -9,11 +9,18 @@ import (
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by loli main(). It only needs to happen once to the rootCmd.
+//
+// Args:
+//   - rootCmd: The root command.
 func Execute() {
+	// Show the header.
 	utils.ShowHeader()
 
+	// Execute the root command.
 	if err := rootCmd.Execute(); err != nil {
+		// Print the error message.
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		// Exit with an error code.
 		os.Exit(1)
 	}
 }
--- commands/completion.go
+++ commands/completion.go
@@ -8,19 +8,30 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var excludeDesc = false
+// excludeDesc is a flag that controls whether or not to include the shell completion
+// description in the generated script.
+var excludeDesc bool
 
+// completionCmd is the command that generates shell completion scripts.
 var completionCmd = &cobra.Command{
-	Use:       "completion <shell>",
-	Short:     "Generate shell completion scripts",
-	Long:      consts.CompletionHelpMessage,
+	// Use is the command's name.
+	Use: "completion <shell>",
+	// Short is a brief description of the command.
+	Short: "Generate shell completion scripts",
+	// Long is a longer description of the command.
+	Long: consts.CompletionHelpMessage,
+	// ValidArgs is a list of the valid shells that the command can generate completion scripts for.
 	ValidArgs: []string{"bash", "zsh", "fish", "powershell"},
+	// Args is a validation function that ensures that the user has only provided one argument,
+	// and that the argument is one of the valid shells.
 	Args: func(cmd *cobra.Command, args []string) error {
 		if cobra.ExactArgs(1)(cmd, args) != nil || cobra.OnlyValidArgs(cmd, args) != nil {
 			return fmt.Errorf("only %v arguments are allowed", cmd.ValidArgs)
 		}
 		return nil
 	},
+	// RunE is the command's main function. It takes the command's arguments and
+	// generates the shell completion script for the specified shell.
 	RunE: func(cmd *cobra.Command, args []string) error {
 		shellType := args[0]
 		out, rootCmd := os.Stdout, cmd.Parent()
@@ -45,7 +56,10 @@ var completionCmd = &cobra.Command{
 	},
 }
 
+// init registers the completionCmd command with the root command.
 func init() {
+	// Flags are defined before the command is added to the root command.
 	completionCmd.Flags().BoolVarP(&excludeDesc, "no-desc", "", false, "Do not include shell completion description")
+	// Add the completionCmd command to the root command.
 	rootCmd.AddCommand(completionCmd)
 }
